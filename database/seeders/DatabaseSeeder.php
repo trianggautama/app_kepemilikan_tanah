bf2288b350a2c125185c0ca6fdfe95ea5b575315
muhammadasadbastomi <tomycules@gmail.com>
2021-06-30 11:11:10 +0800

    create seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'nama' => 'Admin',
+            'username' => 'admin',
+            'password' => Hash::make('123'),
+            'nip' => '123',
+            'jenis_kelamin' => 'Laki-laki',
+            'tempat_lahir' => 'banjarbaru',
+            'tanggal_lahir' => '1997-05-05',
+            'alamat' => 'banjarbaru',
+            'role' => '1',
+        ]);
     }
 }
